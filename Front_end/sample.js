4e0bc51927d12d12080a4984b93d342d774f6f57
Ragulraja313 <ragulraja313@gmail.com>
2024-03-13 18:12:55 +0530

    Javascript

---
+++ Front_end/sample.js
@@ -0,0 +1,26 @@
+// If Statement
+let num = 10;
+
+if (num > 0) {
+    console.log("Number is positive");
+}
+
+// Else Block
+let num1 = -5;
+
+if (num1 > 0) {
+    console.log("Number is positive");
+} else {
+    console.log("Number is non-positive");
+}
+
+// Else if Block
+let num2 = 0;
+
+if (num2 > 0) {
+    console.log("Number is positive");
+} else if (num2 < 0) {
+    console.log("Number is negative");
+} else {
+    console.log("Number is zero");
+}
